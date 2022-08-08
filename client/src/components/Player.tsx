32bfbf86f7b102ee3fdf91a97609574fd2b9e74c
TranDinhTin <tdt.smile1@gmail.com>
2022-08-08 21:30:18 +0700

    fix comment

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,17 +1,17 @@
-// import dateFormat from 'dateformat'
 import { History } from 'history'
-// import update from 'immutability-helper'
-import { Form, Button, Icon, Divider, Image } from 'semantic-ui-react'
+import { Form, Button, Icon, Divider } from 'semantic-ui-react'
 import * as React from 'react'
 import { Grid, Header, Loader } from 'semantic-ui-react'
 import ReactPlayer from 'react-player'
 import { uploadFile } from '../api/todos-api'
 import Auth from '../auth/Auth'
-import { createVideo, deleteVideo, getUploadUrl, getVideos } from '../api/videos-api'
+import {
+  createVideo,
+  deleteVideo,
+  getUploadUrl,
+  getVideos
+} from '../api/videos-api'
 import { Video } from '../types/videos/Video'
-
-// import Auth from '../auth/Auth'
-// import { Todo } from '../types/Todo'
 
 interface PlayersProps {
   auth: Auth
@@ -19,11 +19,12 @@ interface PlayersProps {
 }
 
 interface PlayerState {
-  videos: Video[],
+  videos: Video[]
   loadingVideos: boolean
   file: any
-  uploadState: UploadState,
-  newVideoTitle: string
+  uploadState: UploadState
+  newVideoTitle: string,
+  inputKey: string
 }
 
 enum UploadState {
@@ -33,28 +34,29 @@ enum UploadState {
 }
 
 export class Player extends React.PureComponent<PlayersProps, PlayerState> {
-  // state: PlayerState = {
-  //   loadingVideos: true
-  // }
-
   state: PlayerState = {
     videos: [],
     file: undefined,
     loadingVideos: true,
     uploadState: UploadState.NoUpload,
-    newVideoTitle: ''
+    newVideoTitle: '',
+    inputKey: new Date().toISOString()
   }
 
   async componentDidMount() {
     try {
-      const videos = await getVideos(this.props.auth.getIdToken())
-      this.setState({
-        videos,
-        loadingVideos: false
-      })
+      await this.loadVideo()
     } catch (e) {
       alert(`Failed to fetch todos: ${e}`)
     }
+  }
+
+  async loadVideo() {
+    const videos = await getVideos(this.props.auth.getIdToken())
+    this.setState({
+      videos,
+      loadingVideos: false
+    })
   }
 
   render() {
@@ -68,13 +70,18 @@ export class Player extends React.PureComponent<PlayersProps, PlayerState> {
           <Form onSubmit={this.handleSubmit}>
             <Form.Field>
               <label>Title</label>
-              <input type="text" placeholder="Title" required
+              <input
+                value={this.state.newVideoTitle}
+                type="text"
+                placeholder="Title"
+                required
                 onChange={this.handleTitleChange}
               />
             </Form.Field>
             <Form.Field>
               <label>File</label>
               <input
+                key={this.state.inputKey}
                 type="file"
                 accept="video/mp4,video/x-m4v,video/*"
                 placeholder="Video to upload"
@@ -85,7 +92,6 @@ export class Player extends React.PureComponent<PlayersProps, PlayerState> {
           </Form>
           {this.renderVideos()}
         </div>
-
       </div>
     )
   }
@@ -106,17 +112,20 @@ export class Player extends React.PureComponent<PlayersProps, PlayerState> {
       this.setUploadState(UploadState.FetchingPresignedUrl)
 
       const newVideo = await createVideo(this.props.auth.getIdToken(), {
-        title: this.state.newVideoTitle,
+        title: this.state.newVideoTitle
       })
 
-      console.log(newVideo.title);
-
-      const uploadUrl = await getUploadUrl(this.props.auth.getIdToken(), newVideo.videoId);
+      const uploadUrl = await getUploadUrl(
+        this.props.auth.getIdToken(),
+        newVideo.videoId
+      )
 
       this.setUploadState(UploadState.UploadingFile)
       await uploadFile(uploadUrl, this.state.file)
-
+      this.handleClear()
       alert('File was uploaded!')
+
+      await this.loadVideo()
     } catch (e) {
       alert('Could not upload a file: ' + e)
     } finally {
@@ -194,9 +203,13 @@ export class Player extends React.PureComponent<PlayersProps, PlayerState> {
                   <Icon name="delete" />
                 </Button>
               </Grid.Column>
-              {video.url && (
-                <ReactPlayer url={video.url} /> 
-              )}
+              <ReactPlayer
+                controls
+                className="react-player"
+                url={video.url}
+                width="100%"
+                height="90%"
+              />
               <Grid.Column width={16}>
                 <Divider />
               </Grid.Column>
@@ -211,10 +224,14 @@ export class Player extends React.PureComponent<PlayersProps, PlayerState> {
     try {
       await deleteVideo(this.props.auth.getIdToken(), videoId)
       this.setState({
-        videos: this.state.videos.filter(video => video.videoId !== videoId)
+        videos: this.state.videos.filter((video) => video.videoId !== videoId)
       })
     } catch {
       alert('Video deletion failed')
     }
+  }
+
+  handleClear = () => {
+    this.setState({ newVideoTitle: '', inputKey: new Date().toISOString() })
   }
 }
